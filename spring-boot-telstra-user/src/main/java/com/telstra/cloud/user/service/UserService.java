45e0c18005aec44fc32de6ba63bc7dceaeab7a7f
selvaonp1 <selva.onp@gmail.com>
2017-10-17 21:54:02 +0530

    changes to project names

--- spring-boot-telstra-user/src/main/java/com/telstra/cloud/user/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.telstra.cloud.user.service;
-
-import com.telstra.cloud.user.domain.UserInfo;
-
-
-public interface UserService {
-    public UserInfo getUser(UserSearchCriteria loginModel) throws UserNotFoundException;
-}
